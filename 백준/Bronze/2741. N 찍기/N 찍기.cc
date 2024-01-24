fde696364712a98e94bc7ea3fb5ebb16ca9e8e8a
qmean <77445214+qmean@users.noreply.github.com>
2024-01-24 16:12:16 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,29 @@
+//
+// Created by 김규민 on 2024/01/24.
+//
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+void init()
+{
+
+}
+void solve()
+{
+
+}
+
+int main()
+{
+    int t; cin >> t;
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    for (int i = 0; i < t; ++i) {
+        cout << i + 1 << '\n';
+    }
+}
