2176af9ad7dead99306d76b33a10f71c9538877c
qmean <77445214+qmean@users.noreply.github.com>
2025-04-15 13:38:35 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,23 @@
+//
+// Created by 김규민 on 2024/01/24.
+//
+#include <iostream>
+
+using namespace std;
+
+void init()
+{
+
+}
+void solve()
+{
+
+}
+
+int main()
+{
+    int n; cin >> n;
+    for (int i = 1; i <= 9; ++i) {
+        cout << n << " * " << i << " = " << n * i << endl;
+    }
+}
