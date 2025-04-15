a485731741686cd009cb75173cc9451fc15ca248
qmean <77445214+qmean@users.noreply.github.com>
2025-04-15 13:22:10 +0900

    [Silver III] Title: N과 M (4), Time: 200 ms, Memory: 16192 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		new Solution().solve();
+	}
+
+	private static class Solution {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n, m;
+
+		void init() throws Exception{
+			String[] s = br.readLine().split(" ");
+			n = Integer.parseInt(s[0]);
+			m = Integer.parseInt(s[1]);
+		}
+
+		void solve() throws Exception{
+			init();
+			for (int i = 1; i <= n; i++) {
+				cond(1, i, "");
+			}
+		}
+
+		void cond(int depth, int cur, String str) {
+			if (depth == m) {
+				System.out.println(str + cur);
+				return ;
+			}
+			for (int i = cur; i <= n; i++) {
+				cond(depth + 1, i, str + cur + " ");
+			}
+		}
+	}
+}
