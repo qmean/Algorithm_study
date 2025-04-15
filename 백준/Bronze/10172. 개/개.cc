7574c6155ae023933d7cc31b83552e75e1d914e0
qmean <77445214+qmean@users.noreply.github.com>
2025-04-15 13:37:57 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,19 @@
+//
+// Created by 김규민 on 2024/01/24.
+//
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout <<
+    "|\\_/|\n"
+    "|q p|   /}\n"
+    "( 0 )\"\"\"\\\n"
+    "|\"^\"`    |\n"
+    "||_/=\\\\__|";
+}
