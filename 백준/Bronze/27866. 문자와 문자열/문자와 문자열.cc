4211901168252d8b4c2129eae088694a0990fc75
qmean <77445214+qmean@users.noreply.github.com>
2025-04-15 13:38:25 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,22 @@
+//
+// Created by 김규민 on 2024/01/24.
+//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void init()
+{
+
+}
+void solve()
+{
+
+}
+
+int main()
+{
+    string s; cin >> s; int i; cin >> i;
+    cout << s[i-1] << endl;
+}
