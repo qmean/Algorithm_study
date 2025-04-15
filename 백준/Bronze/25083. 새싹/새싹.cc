a18fd90298fcb6d3493b878ce07ab643c006d3f9
qmean <77445214+qmean@users.noreply.github.com>
2025-04-15 13:38:28 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void init()
+{
+
+}
+void solve()
+{
+
+}
+
+int main()
+{
+    cout << "         ,r'\"7\n"
+            "r`-_   ,'  ,/\n"
+            " \\. \". L_r'\n"
+            "   `~\\/\n"
+            "      |\n"
+            "      |\n";
+}
