14d39e2767ed12bcfc03455ca4914101c53d89be
qmean <77445214+qmean@users.noreply.github.com>
2025-04-15 13:37:44 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,13 @@
+//
+// Created by 김규민 on 2024/01/24.
+//
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int a,b; cin>>a>>b; cout << a*b<<endl;
+}
