3a94375694e154a2ee6afeabc729f33089a399a0
qmean <77445214+qmean@users.noreply.github.com>
2025-04-16 11:54:39 +0900

    Title: 1, 2, 5 더하기, Time: 147ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250416/1, 2, 5 더하기/1-2-5-plus.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        int[] dp = new int[n+1];
+        int[] num = {1, 2, 5};
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[5] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < 3; j++) {
+                int cur = num[j];
+
+                if (i - cur > 0) {
+                    dp[i] += dp[i-cur];
+                }
+            }
+        }
+        
+        System.out.println(dp[n]);
+    }
+}
