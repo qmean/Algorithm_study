e80f15dd849265c22d019aa2fbc4e87f949299e4
qmean <77445214+qmean@users.noreply.github.com>
2025-04-16 11:16:52 +0900

    Title: 동전 거슬러주기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250416/동전 거슬러주기/coin-change.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static int[] dp;
+    public static int[] coin;
+    public static int n;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int m = sc.nextInt();
+        dp = new int[m + 1];
+        coin = new int[n];
+        for (int i = 0; i < n; i++)
+            coin[i] = sc.nextInt();
+        // Please write your code here.
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        if (dp(m) == Integer.MAX_VALUE)
+            System.out.println(-1);
+        else
+            System.out.println(dp[m]);
+    }
+
+    public static int dp(int m) {
+        for(int i = 0; i < coin.length; i++) {
+            if (m - coin[i] >= 0)
+                dp[m] = Math.min(dp[m], dp(m-coin[i]) + 1);
+        }
+        return dp[m];
+    }
+}
