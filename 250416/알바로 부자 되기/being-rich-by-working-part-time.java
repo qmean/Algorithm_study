2e01217f214eb46eaa57f3fb894183b865261422
qmean <77445214+qmean@users.noreply.github.com>
2025-04-16 10:49:24 +0900

    Title: 알바로 부자 되기, Time: 279ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250416/알바로 부자 되기/being-rich-by-working-part-time.java
+++ 250416/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -12,25 +12,25 @@ public class Main {
             p[i] = sc.nextInt();
         }
         // Please write your code here.
-        int[] dp = new int[1001];
+        Map<Integer, Integer> map = new HashMap<>();
 
-        Arrays.fill(dp, Integer.MIN_VALUE);
         for(int i = 0; i < n; i++) {
             int iStart = s[i];
             int iPay = p[i];
-            dp[iStart] = Math.max(dp[iStart], p[i]);
+            map.put(iStart, Math.max(map.getOrDefault(iStart, -1), p[i]));
             for(int j = 0; j < i; j++) {
                 int jEnd = e[j];
                 int jStart = s[j];
                 if (jEnd < iStart) {
-                    dp[iStart] = Math.max(dp[iStart], dp[jStart] + iPay);
+                    map.put(iStart, Math.max(map.get(iStart), map.get(jStart) + iPay));
                 }
             }
         }
 
         int max = Integer.MIN_VALUE;
-        for(int i = 1; i <= 1000; i++) {
-            max = Math.max(max, dp[i]);
+        Set<Map.Entry<Integer, Integer>> set = map.entrySet();
+        for(Map.Entry<Integer, Integer> entry : set) {
+            max = Math.max(entry.getValue(), max);
         }
 
         // for(int i = 1; i <= 100; i++) {
