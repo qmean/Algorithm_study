3213fb33816949635eb88666705fa7507131475d
qmean <77445214+qmean@users.noreply.github.com>
2025-04-16 11:55:49 +0900

    Title: 1, 2, 5 더하기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250416/1, 2, 5 더하기/1-2-5-plus.java
+++ 250416/1, 2, 5 더하기/1-2-5-plus.java
@@ -13,13 +13,11 @@ public class Main {
         for(int i = 1; i <= n; i++) {
             for(int j = 0; j < 3; j++) {
                 int cur = num[j];
-
                 if (i - cur > 0) {
-                    dp[i] += dp[i-cur];
+                    dp[i] = (dp[i-cur] + dp[i]) % 10007;
                 }
             }
         }
-        
         System.out.println(dp[n]);
     }
 }
