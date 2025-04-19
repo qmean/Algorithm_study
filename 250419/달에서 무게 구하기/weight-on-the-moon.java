181fd0917e1fd97775a9c2a282a45ffb65c7b937
qmean <77445214+qmean@users.noreply.github.com>
2025-04-19 13:47:02 +0900

    Title: 달에서 무게 구하기, Time: 104ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250419/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int w = 13;
+        double b = 0.165;
+
+        System.out.format("%d * %6f = %6f", w, b, w*b);
+    }
+}
