8c76f9a9cf48038483ee6b8e881e3580787a7883
qmean <77445214+qmean@users.noreply.github.com>
2025-04-19 13:55:44 +0900

    Title: 실수 받아 그대로 출력, Time: 151ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250419/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+
+        System.out.format("%.2f", n);
+    }
+}
