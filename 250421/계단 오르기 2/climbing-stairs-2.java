7a8c206504f6ef7c7907d22008d3416d2fa2014e
qmean <77445214+qmean@users.noreply.github.com>
2025-04-21 12:42:05 +0900

    Title: 계단 오르기 2, Time: 165ms, Memory: 11MB, Status: Passed - Codetree

--- 250421/계단 오르기 2/climbing-stairs-2.java
+++ 250421/계단 오르기 2/climbing-stairs-2.java
@@ -9,11 +9,28 @@ public class Main {
         }
         // Please write your code here.
         int[][] dp = new int[n+1][4];
+        for(int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        dp[0][0] = 0;
         dp[1][1] = coins[0];
         for(int i = 2; i <= n; i++) {
-            dp[i][0] = dp[i-2][0] + coins[i-1];
+            // 짝수일 경우에만 업데이트
+            if (i % 2 != 1)
+                dp[i][0] = dp[i-2][0] + coins[i-1];
             for(int j = 1; j <= 3; j++) {
-                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-2][j]) + coins[i-1];
+                // (번호 + 1 개수)가 홀수이면 불가능한 수
+                if ((i + j) % 2 != 1) {
+                    if (dp[i-1][j-1] < 0 && dp[i-2][j] < 0) {
+                        continue;
+                    } else if (dp[i-1][j-1] < 0) {
+                        dp[i][j] = dp[i-2][j] + coins[i-1];
+                    } else if (dp[i-2][j] < 0) {
+                        dp[i][j] = dp[i-1][j-1] + coins[i-1];
+                    } else {
+                        dp[i][j] = Math.max(dp[i-1][j-1], dp[i-2][j]) + coins[i-1];
+                    }
+                }
             }
         }
 
@@ -36,4 +53,16 @@ dp[i][0] = dp[i-2][0] + coin[i];
 dp[i][1] = max(dp[i-1][0] , dp[i-2][1]) + coin[i]
 dp[i][2] = max(dp[i-1][1] , dp[i-2][2]) + coin[i]
 dp[i][3] = max(dp[i-1][2] , dp[i-2][3]) + coin[i]
+
+불가능한 경우
+1,0
+1,2
+1,3
+2,1
+2,2
+2,3
+3,0
+3,2
+4,1
+4,3
 */
