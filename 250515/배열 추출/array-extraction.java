27a1a6111f70d79bf3e3d59cbb73b5377d5d91f0
qmean <77445214+qmean@users.noreply.github.com>
2025-05-15 14:10:28 +0900

    Title: 배열 추출, Time: 1446ms, Memory: 54MB, Status: Passed - Codetree

---
+++ 250515/배열 추출/array-extraction.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Queue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            // Please write your code here.
+            if (x != 0) {
+                q.add(x);
+            } else {
+                if (q.isEmpty())
+                    System.out.println(0);
+                else
+                    System.out.println(q.poll());
+            }
+        }
+    }
+}
