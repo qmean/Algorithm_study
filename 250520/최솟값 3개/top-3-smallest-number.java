860448d071ff955166ee9c20961eaf5d86abb7f7
qmean <77445214+qmean@users.noreply.github.com>
2025-05-20 08:23:02 +0900

    Title: 최솟값 3개, Time: 862ms, Memory: 25MB, Status: Passed - Codetree

--- 250520/최솟값 3개/top-3-smallest-number.java
+++ 250520/최솟값 3개/top-3-smallest-number.java
@@ -13,7 +13,8 @@ public class Main {
                 int i1 = pq.poll();
                 int i2 = pq.poll();
                 int i3 = pq.poll();
-                long ans = i1 * i2 * i3;
+                long ans = (long)i1 * (long)i2;
+                ans *= (long)i3;
                 System.out.println(ans);
                 pq.add(i1);
                 pq.add(i2);
