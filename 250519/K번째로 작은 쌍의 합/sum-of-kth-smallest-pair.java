a1c28b37ef91e52e5617ec7ffca03e7f7f2a8966
qmean <77445214+qmean@users.noreply.github.com>
2025-05-19 11:49:11 +0900

    Title: K번째로 작은 쌍의 합, Time: 130ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250519/K번째로 작은 쌍의 합/sum-of-kth-smallest-pair.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] nArr = new int[n];
+        int[] mArr = new int[m];
+
+        for(int i = 0; i < n; i++) {
+            nArr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < m; i++) {
+            mArr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(nArr);
+        Arrays.sort(mArr);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        int np = 0;
+        while (true) {
+            int curN = nArr[np];
+            if (pq.size() == k && pq.peek() < curN + mArr[0]) {
+                break;
+            }
+            for(int i = 0; i < m; i++) {
+                pq.add(curN + mArr[i]);
+                if (pq.size() > k) {
+                    pq.poll();
+                }
+            }
+            np++;
+        }
+
+        // pq.forEach(i -> System.out.println(i));
+
+        System.out.println(pq.peek());
+    }
+}
