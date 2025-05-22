0626f7dd9bb4247b202c63a340da5c71129572f4
qmean <77445214+qmean@users.noreply.github.com>
2025-05-22 13:39:14 +0900

    Title: 점 개수 세기, Time: 83ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250522/점 개수 세기/count-number-of-points.java
+++ 250522/점 개수 세기/count-number-of-points.java
@@ -1,23 +1,31 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int Q = sc.nextInt();
-        TreeSet<Integer> nums = new TreeSet<>();
+    public static void main(String[] args) throws Exception {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+        TreeSet<Integer> set = new TreeSet<>();
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            nums.add(sc.nextInt());
+            set.add(Integer.parseInt(st.nextToken()));
         }
         Map<Integer, Integer> map = new HashMap<>();
+        TreeSet<Integer> nums = new TreeSet<>();
         int idx = 1;
-        for (Integer i : nums) {
+        for (Integer i : set) {
             map.put(i, idx);
+            nums.add(i);
             idx++;
         }
         for (int i = 0; i < Q; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
             Integer ceiling = nums.ceiling(a);
             Integer floor = nums.floor(b);
@@ -28,7 +36,12 @@ public class Main {
             int min = map.get(ceiling);
             int max = map.get(floor);
 
-            System.out.println(max - min + 1);
+            bw.write(String.valueOf(max - min + 1) + "\n");
+            if (i % 10 == 0) {
+                bw.flush();
+            }
         }
+        bw.flush();
+        bw.close();
     }
 }
