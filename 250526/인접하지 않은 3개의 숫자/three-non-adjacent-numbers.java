5ddc174b2d110d80e889b6fb2d4578f1f97f21ac
qmean <77445214+qmean@users.noreply.github.com>
2025-05-26 16:29:39 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 617ms, Memory: 33MB, Status: Passed - Codetree

--- 250526/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.java
+++ 250526/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.java
@@ -16,13 +16,9 @@ public class Main {
         for(int i = 1; i < n; i++) {
             l[i] = Math.max(l[i-1], arr[i]);
         }
-        r[0] = arr[n-1];
+        r[n-1] = arr[n-1];
         for(int i = n-2; i >= 0; i--) {
             r[i] = Math.max(r[i+1], arr[i]);
-        }
-
-        for(int i = 0; i < n; i++) {
-            System.out.print(l[i] + " ");
         }
 
         int max = Integer.MIN_VALUE;
