6e80896117d1bf466dcc93a93c07951a613fd37f
qmean <77445214+qmean@users.noreply.github.com>
2025-07-24 10:34:16 +0900

    Title: 최대 길이를 위해 선분 제거하기, Time: 423ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250724/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // 삭제되었을 경우 수직선상에서 없어지는 총 길이
+        int[] delValue = new int[n];
+        List<Point> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            list.add(new Point(x1, 1, i));
+            list.add(new Point(x2, -1, i));
+        }
+        list.sort((p1, p2) -> {
+            if (p1.x == p2.x) {
+                return p2.v - p1.v;
+            } else {
+                return p1.x - p2.x;
+            }
+        });
+
+        int sum = 0;
+        int sumStart = 0;
+        int idxStart = 0;
+        int curIdx = 0;
+        Set<Integer> set = new HashSet<>();
+        
+        for(Point p : list) {
+            if (p.v == 1) {
+                if (set.isEmpty()) {
+                    sumStart = p.x;
+                    idxStart = p.x;
+                    curIdx = p.idx;
+                }
+                else if (set.size() == 1) {
+                    delValue[set.iterator().next()] += idxStart - p.x;
+                }
+                set.add(p.idx);
+            } 
+            else {
+                set.remove(p.idx);
+                if (set.isEmpty()) {
+                    sum += p.x - sumStart;
+                    delValue[curIdx] += p.x - idxStart;
+                } else if (set.size() == 1) {
+                    delValue[curIdx] += p.x - idxStart;
+                    curIdx = set.iterator().next();
+                    idxStart = p.x;
+                }
+            }
+            // System.out.format("p.x : %d p.v : %d p.idx = %d idxStart = %d curIdx = %d\n", p.x, p.v, p.idx, idxStart, curIdx);
+        }
+
+        int min = Integer.MAX_VALUE;
+        // System.out.println("=====");
+        for(int i : delValue) {
+            // System.out.println(i);
+            min = Math.min(i, min);
+        }
+        System.out.println(sum - min);
+    }
+
+    public static class Point {
+        int idx;
+        int x;
+        int v;
+        public Point(int x, int v, int idx) {
+            this.x = x;
+            this.v = v;
+            this.idx = idx;
+        }
+    }
+}
