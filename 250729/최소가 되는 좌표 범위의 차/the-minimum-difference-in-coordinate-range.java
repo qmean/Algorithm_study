fe386c64c3c8e14a964937c0bbda7b231c7223ca
qmean <77445214+qmean@users.noreply.github.com>
2025-07-29 11:30:24 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 371ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250729/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.java
+++ 250729/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.java
@@ -16,35 +16,42 @@ public class Main {
         
         int min = Integer.MAX_VALUE;
         list.sort((p1, p2) -> {
-            if (p1.x == p2.x) {
-                return p1.y - p2.y;
-            }
-            return p1.x - p2.x;
+            return p1.y - p2.y;
+            // if (p1.x == p2.x) {
+            //     return p1.y - p2.y;
+            // }
+            // return p1.x - p2.x;
         });
         int ymin = Integer.MAX_VALUE;
         int ymax = Integer.MIN_VALUE;
         int a;
         int b = -1;
-        int j = 0;
-        TreeSet<Integer> set = new TreeSet<>();
+        int j = 1;
+        TreeSet<Point> set = new TreeSet<>((p1, p2) -> {
+            if (p1.y == p2.y) {
+                return p1.x - p2.x;
+            }
+            return p1.y - p2.y;
+        });
         for(int i = 0; i < n; i++) {
             Point ip = list.get(i);
             a = ip.x;
-            set.add(ip.y);
+            set.add(ip);
             // System.out.format("====== i : %d j : %d ymax : %d ymin : %d\n", i, j, ymax, ymin);
-            while(j < n && set.last() - set.first() < d) {
-                // System.out.format("i : %d j : %d ymax : %d ymin : %d\n", i, j, ymax, ymin);
+            while(j < n && set.last().y - set.first().y < d) {
                 Point jp = list.get(j);
-                set.add(jp.y);
+                set.add(jp);
+                // System.out.format("i : %d j : %d ymax : %d ymin : %d\n", i, j, map.firstKey(), map.lastKey());
                 b = jp.x;
                 j++;
             }
-            if (set.last() - set.first() >= d && set.size() != 1) {
-                // System.out.format("****** i : %d j : %d ymax : %d ymin : %d\n", i, j, ymax, ymin);
-                // b = list.get(j).x;
+            if (set.last().y - set.first().y >= d && set.size() != 1) {
+                // System.out.println(a + " " + b);
+                // System.out.format("===== i : %d j : %d ymax : %d ymin : %d\n", i, j, map.firstKey(), map.lastKey());
                 min = Math.min(Math.abs(b-a), min);
             }
-            set.remove(ip.y);
+            
+            set.remove(ip);
         }
 
         min = min == Integer.MAX_VALUE ? -1 : min;
