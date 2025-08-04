6b2474d26573b98da962d32904cf1afe7c9efb57
qmean <77445214+qmean@users.noreply.github.com>
2025-08-04 14:32:02 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 250804/양쪽 모두 존재하는 구간/both-exist-section.java
+++ 250804/양쪽 모두 존재하는 구간/both-exist-section.java
@@ -18,8 +18,9 @@ public class Main {
             }
         }
         
-        int j = 0;
+        
         int min = Integer.MAX_VALUE;
+        int j = 0;
         for (int i = 1; i <= n; i++) {
             while(j + 1 < n && inSet.size() < m) {
                 if (1 <= arr[j+1] && arr[j+1] <= m) {
@@ -47,6 +48,49 @@ public class Main {
                 inSet.remove(arr[i]);
             }
         }
+
+        inSet.clear();
+        outSet.clear();
+        inMap.clear();
+        outMap.clear();
+        for (int i = 1; i <= n; i++) {
+            if (0 < arr[i] && arr[i] <= m) {
+                outMap.put(arr[i], outMap.getOrDefault(arr[i], 0) + 1);
+                outSet.add(arr[i]);
+                inMap.put(arr[i], 0);
+            }
+        }
+
+        j = n+1;
+        for (int i = n; i >= 1; i--) {
+            while(j - 1 > 0 && inSet.size() < m) {
+                if (1 <= arr[j-1] && arr[j-1] <= m) {
+                    inMap.put(arr[j-1], inMap.get(arr[j-1]) + 1);
+                    inSet.add(arr[j-1]);
+                    outMap.put(arr[j-1], outMap.get(arr[j-1]) - 1);
+                    if (outMap.get(arr[j-1]) == 0) {
+                        outSet.remove(arr[j-1]);
+                    }
+                }
+                j--;
+            }
+
+            if (inSet.size() == m && outSet.size() == m) {
+                min = Math.min(min, i - j + 1);
+            }
+            
+            // System.out.format("=====i : %d j : %d =====\n", i, j);
+            // System.out.println("inMap\n" + inMap);
+            // System.out.println("outMap\n" + outMap);
+            outMap.put(arr[i], outMap.get(arr[i]) + 1);
+            outSet.add(arr[i]);
+            inMap.put(arr[i], inMap.get(arr[i])- 1);
+            if (inMap.get(arr[i]) == 0) {
+                inSet.remove(arr[i]);
+            }
+        }
+
+
         if (min != Integer.MAX_VALUE)
             System.out.println(min);
         else
