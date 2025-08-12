e3cdd3eafa4db6dab16c891f14d5b35c64d57682
qmean <77445214+qmean@users.noreply.github.com>
2025-08-12 11:25:14 +0900

    Title: 정수 분배하기, Time: 114ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250812/정수 분배하기/distributing-integers.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            max = Math.max(max, arr[i]);
+        }
+        int start = 1;
+        int end = max;
+        // 정수 k
+        int mid = 1;
+        int ans = Integer.MIN_VALUE;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            int cnt = 0;
+            for(int i : arr) {
+                cnt += i / mid;
+            }
+            if (cnt >= m) {
+                start = mid + 1;
+                ans = Math.max(ans, mid);
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        System.out.format("%d\n", ans);
+    }
+}
