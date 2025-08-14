4b25d99cd83e8a9cb2c177923a480595073eba5c
qmean <77445214+qmean@users.noreply.github.com>
2025-08-14 11:30:24 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 1351ms, Memory: 45MB, Status: Runtime Error - Codetree

---
+++ 250814/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        Gem[] arr = new Gem[n];
+        for (int i = 0; i < n; i++) {
+            int w = sc.nextInt();
+            int v = sc.nextInt();
+            arr[i] = new Gem(w,v);
+        }
+        Arrays.sort(arr, (g1, g2) -> {
+            if (g1.t < g2.t) {
+                return 1;
+            } else if (g1.t > g2.t) {
+                return -1;
+            }
+            return 0;
+        });
+        double sum = 0;
+        int mount = 0;
+        int idx = 0;
+        while(mount != m) {
+            Gem cur = arr[idx];
+            idx++;
+            int curW = Math.min(m - mount, cur.w);
+            sum += cur.t * curW;
+            mount += curW;
+        }
+        System.out.format("%.3f\n", sum);
+    }
+
+    public static class Gem {
+        int w;
+        int v;
+        double t;
+        
+        public Gem(int w, int v) {
+            this.w = w;
+            this.v = v;
+            this.t = (double)v/(double)w;
+        }
+    }
+}
