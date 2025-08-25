ee9f048a88c2b87107c6ba569c4e44034df6be4f
qmean <77445214+qmean@users.noreply.github.com>
2025-08-25 13:14:35 +0900

    Title: 정점 연결하기, Time: 973ms, Memory: 37MB, Status: Wrong Answer - Codetree

---
+++ 250825/정점 연결하기/connect-vertices.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Main {
+
+    static int[] uf;
+    static Set<Integer> set;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] edges = new int[n - 2][2];
+        set = new HashSet<>();
+        uf = new int[n+1];
+        for(int i = 1; i <= n; i++) {
+            uf[i] = i;
+            set.add(i);
+        }
+        for (int i = 0; i < n - 2; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            union(a,b);
+        }
+        set.stream().sorted(Comparator.naturalOrder()).forEach(i -> System.out.print(i + " "));
+        System.out.println();
+    }
+
+    static int find(int d) {
+        if (uf[d] == d)
+            return d;
+        uf[d] = find(uf[d]);
+        return uf[d];
+    }
+
+    static void union(int a, int b) {
+        int ar = find(a);
+        int br = find(b);
+        if (a < b) {
+            uf[br] = ar;
+            set.remove(br);
+        } else if (a > b) {
+            uf[ar] = br;
+            set.remove(ar);
+        }
+    }
+}
