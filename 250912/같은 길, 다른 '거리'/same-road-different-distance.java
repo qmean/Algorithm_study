c5642acf32506b4e8b208f6464101b27bb5b6bf0
qmean <qmean59@gmail.com>
2025-09-12 09:11:39 +0900

    Title: 같은 길, 다른 '거리', Time: 1190ms, Memory: 54MB, Status: Wrong Answer - Codetree

--- 250912/같은 길, 다른 '거리'/same-road-different-distance.java
+++ 250912/같은 길, 다른 '거리'/same-road-different-distance.java
@@ -3,24 +3,19 @@ import java.util.*;
 public class Main {
 
     static ArrayList<Edge>[] edges;
-    static Set<Edge>[] toA;
-    static Set<Edge>[] toB;
+    static ArrayList<Edge>[] edges2;
     static int[] dp;
-    static int[][] warn;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         dp = new int[n];
-        warn = new int[n][n];
-        toA = new HashSet[n];
-        toB = new HashSet[n];
         edges = new ArrayList[n];
+        edges2 = new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            toA[i] = new HashSet<>();
-            toB[i] = new HashSet<>();
             edges[i] = new ArrayList<>();
+            edges2[i] = new ArrayList<>();
         }
         
         int m = sc.nextInt();
@@ -30,74 +25,41 @@ public class Main {
             int costA = sc.nextInt();
             int costB = sc.nextInt();
 
-            edges[start].add(new Edge(end, costA, costB));
+            Edge e = new Edge(start, end, costA, costB);
+            edges[start].add(e);
+            edges2[end].add(e);
         }
         
+        for (int i = 0; i < n; i++) {
+            Edge amin = null;
+            Edge bmin = null;
+            for (Edge e : edges2[i]) {
+                if (amin == null || amin.ac > e.ac) {
+                    amin = e;
+                }
+                if (bmin == null || bmin.bc > e.bc) {
+                    bmin = e;
+                }
+            }
+            if (amin != null)
+                amin.counter -= 1;
+            if (bmin != null)
+                bmin.counter -= 1;
+        }
+
+        // for (int i = 0; i < n; i++) {
+        //     System.out.println("====" + (i+1) + "====");
+        //     for (Edge e : edges2[i])
+        //         System.out.format("src = %d dest = %d counter = %d\n",e.idx+1, e.src+1, e.counter);
+        // }
+        
         Arrays.fill(dp, (int)1e9);
+        dp[n-1] = 0;
         PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> n1.v - n2.v);
-        dp[n-1] = 0;
         pq.add(new Node(n-1, 0));
         while(!pq.isEmpty()) {
             Node cur = pq.poll();
             
-            if (dp[cur.idx] != cur.v)
-                continue;
-
-            for (Edge e : edges[cur.idx]) {
-                int nv = e.ac + cur.v;
-                if (dp[e.idx] >= nv) {
-                    dp[e.idx] = nv;
-                    if (dp[e.idx] != nv) {
-                        toA[e.idx].clear();
-                    }
-                    toA[cur.idx].add(e);
-                    pq.add(new Node(e.idx, nv));
-                }
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (Edge e : toA[i]) {
-                e.counter = e.counter - 1;
-                // System.out.println(e.counter);
-            }
-        }
-
-        Arrays.fill(dp, (int)1e9);
-        dp[n-1] = 0;
-        pq.add(new Node(n-1, 0));
-        while(!pq.isEmpty()) {
-            Node cur = pq.poll();
-            
-            if (dp[cur.idx] != cur.v)
-                continue;
-
-            for (Edge e : edges[cur.idx]) {
-                int nv = e.bc + cur.v;
-                if (dp[e.idx] >= nv) {
-                    dp[e.idx] = nv;
-                    if (dp[e.idx] != nv) {
-                        toB[e.idx].clear();
-                    }
-                    toB[e.idx].add(e);
-                    pq.add(new Node(e.idx, nv));
-                }
-            }
-        }
-        
-        // System.out.println();
-        for (int i = 0; i < n; i++) {
-            for (Edge e : toB[i]) {
-                e.counter = e.counter - 1;
-                // System.out.println(e.counter);
-            }
-        }
-
-        dp[n-1] = 0;
-        pq.add(new Node(n-1, 0));
-        while(!pq.isEmpty()) {
-            Node cur = pq.poll();
-
             if (dp[cur.idx] != cur.v) continue;
 
             for (Edge e : edges[cur.idx]) {
@@ -108,17 +70,22 @@ public class Main {
                 }
             }
         }
+        // for(int i = 0; i < n; i++)
+        //     System.out.print(dp[i] + " ");
+        // System.out.println();
 
         System.out.println(dp[0]);
     }
 
     static class Edge implements Comparable<Edge> {
         int idx;
+        int src;
         int ac;
         int bc;
         int counter = 2;
 
-        public Edge(int idx, int ac, int bc) {
+        public Edge(int src, int idx, int ac, int bc) {
+            this.src = src;
             this.idx = idx;
             this.ac = ac;
             this.bc = bc;
@@ -126,7 +93,7 @@ public class Main {
 
         @Override
         public int compareTo(Edge e) {
-            if (this.idx == e.idx && this.ac == e.ac && this.bc == e.bc)
+            if (this.src == e.src && this.idx == e.idx && this.ac == e.ac && this.bc == e.bc)
                 return 0;
             return 1;
         }
