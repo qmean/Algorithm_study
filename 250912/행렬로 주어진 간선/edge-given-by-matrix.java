237e51644b1a1e4376206154fc66fc199720f6bb
qmean <qmean59@gmail.com>
2025-09-12 16:58:49 +0900

    Title: 행렬로 주어진 간선, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250912/행렬로 주어진 간선/edge-given-by-matrix.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+            matrix[i][i] = 1;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (matrix[j][i] == 1 && matrix[i][k] == 1)
+                        matrix[j][k] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
