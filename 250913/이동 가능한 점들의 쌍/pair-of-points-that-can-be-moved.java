730de25b54831561b91a8bbd7b3e105cb46816b2
qmean <qmean59@gmail.com>
2025-09-13 14:41:13 +0900

    Title: 이동 가능한 점들의 쌍, Time: 316ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250913/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.java
+++ 250913/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.java
@@ -14,32 +14,44 @@ public class Main {
         int Q = sc.nextInt();
 
         map = new int[n][n];
+        ans = new int[n][n];
         red = new boolean[n][n];
         for (int i = 0; i < n; i++) {
             Arrays.fill(map[i], (int)1e9);
+            Arrays.fill(ans[i], (int)1e9);
             map[i][i] = 0;
         }
 
         for (int i = 0; i < P; i++) {
-            red[i][i] = true;
+            ans[i][i] = 0;
+            // red[i][i] = 0;
         }
-
+            
         for (int i = 0; i < M; i++) {
             int u = sc.nextInt() - 1;
             int v = sc.nextInt() - 1;
             int w = sc.nextInt();
-            if (red[u][u] || red[v][v]) {
-                red[u][v] = true;
+            if (u <= P || v <= P) {
+                ans[u][v] = Math.min(ans[u][v], w);
             }
             map[u][v] = Math.min(map[u][v], w);
         }
 
-        for (int i = 0 ; i < P; i++) {
+        for (int i = 0 ; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < n; k++) {
                     map[j][k] = Math.min(map[j][k], map[j][i] + map[i][k]);
+                    ans[j][k] = Math.min(ans[j][k], ans[j][i] + ans[i][k]);
+                }
+            }
+        }
+
+        for (int i = 0 ; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    // map[j][k] = Math.min(map[j][k], map[j][i] + map[i][k]);
                     if (i < P || j < P || k < P)
-                        red[j][k] = true;
+                        ans[j][k] = Math.min(ans[j][k], map[j][i] + map[i][k]);
                 }
             }
         }
@@ -50,8 +62,8 @@ public class Main {
             int x = sc.nextInt() - 1;
             int y = sc.nextInt() - 1;
             
-            if (map[x][y] != (int)1e9 && red[x][y]) {
-                sum += map[x][y];
+            if (ans[x][y] != (int)1e9) {
+                sum += ans[x][y];
                 cnt += 1;
             }
         }
