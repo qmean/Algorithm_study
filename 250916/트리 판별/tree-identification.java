036cec8d450de0cf97bad734eb856067ba5c26dc
qmean <qmean59@gmail.com>
2025-09-16 08:04:06 +0900

    Title: 트리 판별, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

--- 250916/트리 판별/tree-identification.java
+++ 250916/트리 판별/tree-identification.java
@@ -3,6 +3,8 @@ public class Main {
 
     static ArrayList<Integer>[] edges;
     static int[] indegree;
+    static boolean[] visited;
+    static Set<Integer> nodes;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,6 +12,8 @@ public class Main {
         
         edges = new ArrayList[10000];
         indegree = new int[10000];
+        visited = new boolean[10000];
+        nodes = new HashSet<>();
 
         for (int i = 0; i < 10000; i++)
             edges[i] = new ArrayList<>();
@@ -18,6 +22,8 @@ public class Main {
             int a = sc.nextInt() - 1;
             int b = sc.nextInt() - 1;
 
+            nodes.add(a);
+            nodes.add(b);
             edges[a].add(b);
             indegree[b]++;
         }
@@ -25,24 +31,46 @@ public class Main {
         int root = -1;
         int rootCnt = 0;
         for (int i = 0; i < 10000; i++) {
-            if (indegree[i] >= 2) {
+            if (indegree[i] == 0 && edges[i].size() == 0) {
+                continue ;
+            } else if (indegree[i] == 0 && edges[i].size() != 0) {
+                root = i;
+                rootCnt++;
+            } else if (indegree[i] >= 2) {
+                // System.out.println("indegree >= 2");
                 System.out.println(0);
                 return;
             }
-            if (indegree[i] == 0 && edges[i].size() != 0) {
-                root = i;
-                rootCnt++;
-            }
+        }
+        
+        if (rootCnt != 1) {
+            // System.out.println("root cnt != 1");
+            System.out.println(0);
+            return;
         }
 
-        if (root == -1) {
+        dfs(root);
+
+        int cnt = 0;
+        for (int i = 0; i < 10000; i++) {
+            if (visited[i])
+                cnt++;
+        }
+
+        if (cnt != nodes.size()) {
             System.out.println(0);
-            return;
-        } else if (rootCnt != 1) {
-            System.out.println(0);
-            return;
+            return ;
         }
 
         System.out.println(1);
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        
+        for (int i : edges[node]) {
+            if (!visited[i])
+                dfs(i);
+        }
     }
 }
