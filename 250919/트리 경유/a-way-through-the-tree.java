d99db8bb58511cfed666fb6cab9f72a49f3396ea
qmean <qmean59@gmail.com>
2025-09-19 14:21:43 +0900

    Title: 트리 경유, Time: 3013ms, Memory: 96MB, Status: Time Limit Exceed - Codetree

--- 250919/트리 경유/a-way-through-the-tree.java
+++ 250919/트리 경유/a-way-through-the-tree.java
@@ -1,8 +1,10 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int q = sc.nextInt();
@@ -19,10 +21,11 @@ public class Main {
                 }
                 find /= 2;
             }
-            System.out.println(print);
+            bw.write(String.valueOf(print) + "\n");
             if (print == 0) {
                 visited.add(a);
             }
+            bw.flush();
         }
     }
     
