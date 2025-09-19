93538b5a4df27d9ed2d3a72082bd6740f3cbd052
qmean <qmean59@gmail.com>
2025-09-19 14:17:15 +0900

    Title: 트리 경유, Time: 3015ms, Memory: 72MB, Status: Time Limit Exceed - Codetree

--- 250919/트리 경유/a-way-through-the-tree.java
+++ 250919/트리 경유/a-way-through-the-tree.java
@@ -1,15 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
-    static boolean[] visited;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int q = sc.nextInt();
 
-        visited = new boolean[n+1];
+        Set<Integer> visited = new HashSet<>();
         for (int i = 0; i < q; i++) {
             int a = sc.nextInt();
             int find = a;
@@ -17,16 +15,15 @@ public class Main {
             boolean valid = true;
             int lastNode = -1;
             while (find != 1) {
-                if (visited[find]) {
-                    valid = false;
+                if (visited.contains(find)) {
                     lastNode = find;
-                    break;
+                    valid = false;
                 }
                 find /= 2;
             }
             if (valid) {
                 System.out.println(0);
-                visited[a] = true;
+                visited.add(a);
             }
             else
                 System.out.println(lastNode);
